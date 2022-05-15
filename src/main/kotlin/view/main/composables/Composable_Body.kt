4a25eb21d0b3af3b47c94a4417d11de0b717903a
plNav <83410074+plNav@users.noreply.github.com>
2022-05-15 21:04:39 +0200

    Main Screen Upgraded - Nodes Conflicts Starting

--- src/main/kotlin/view/main/composables/Composable_Body.kt
+++ src/main/kotlin/view/main/composables/Composable_Body.kt
@@ -1,15 +1,25 @@
 package view.main.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -17,6 +27,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 import data.model.product_stock.product_stock_response
+import kotlinx.coroutines.launch
+import theme.Spacing
 
 import utils.excludedFamilies
 
@@ -25,6 +37,13 @@ fun Body(
     familyProducts: HashMap<String, List<product_stock_response>>,
     popUpDetailsOpen: MutableState<Boolean>
 ) {
+
+    val verticalGradientDisabled = Brush.verticalGradient(
+        colors = listOf(MaterialTheme.colors.onError, Color.DarkGray),
+        startY = 0f,
+        endY = 100f
+    )
+
     LazyColumn{
         itemsIndexed(familyProducts.keys.toMutableList()) { index, family ->
             Column(
@@ -43,25 +62,47 @@ fun Body(
                             .fillMaxWidth()
                             .padding(start = 10.dp, top = 5.dp)
                     )
-                    LazyRow(
-                        /*count = familyProducts.get(family)?.size ?: 2,
-                        contentPadding = PaddingValues(horizontal = 64.dp, vertical = 10.dp),
-                        //flingBehavior = PagerDefaults.flingBehavior(),*/
+                    var count = 0
+                    val row = mutableListOf<product_stock_response>()
+                    for(product in familyProducts.get(family)!!){
 
-                    //state = rememberPagerState(),
-                        modifier = Modifier
-                            .height(400.dp) //TODO DEPEND ON SCREEN SIZE
-                    ) {
-                        for(product in familyProducts.get(family)!!){
-                            item{
-                                ImageContent(
-                                    product = product,
-                                    familyProducts = familyProducts,
-                                    family = family,
-                                    index = index,
-                                    popUpDetailsOpen = popUpDetailsOpen
-                                )
+                        count++
+                        row.add(product)
+                       if(count == 6){
+                            Row(
+                                modifier = Modifier.background(verticalGradientDisabled)
+                            ) {
+                                for(prod in row){
+                                    Box{
+                                        ImageContent(
+                                            product = prod,
+                                            familyProducts = familyProducts,
+                                            family = family,
+                                            index = index,
+                                            popUpDetailsOpen = popUpDetailsOpen
+                                        )
+                                    }
+                                }
+                                row.clear()
                             }
+                        }
+                    }
+                    if(row.isNotEmpty()) {
+                        Row(
+                            modifier = Modifier.background(verticalGradientDisabled)
+                        ) {
+                            for (p in row) {
+                                Box {
+                                    ImageContent(
+                                        product = p,
+                                        familyProducts = familyProducts,
+                                        family = family,
+                                        index = index,
+                                        popUpDetailsOpen = popUpDetailsOpen
+                                    )
+                                }
+                            }
+                            row.clear()
                         }
                     }
                 }
--- src/main/kotlin/view/main/composables/Composable_ImageContent.kt
+++ src/main/kotlin/view/main/composables/Composable_ImageContent.kt
@@ -1,14 +1,20 @@
 package view.main.composables
 
+import androidx.compose.foundation.background
 import utils.AsyncImage
 import utils.loadImageBitmap
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.unit.dp
 import data.model.product_stock.product_stock_response
@@ -23,9 +29,14 @@ fun ImageContent(
     index: Int,
     popUpDetailsOpen: MutableState<Boolean>
 ) {
+    val verticalGradientDisabled = Brush.verticalGradient(
+        colors = listOf(MaterialTheme.colors.onError, Color.DarkGray),
+        startY = 0f,
+        endY = 100f
+    )
     Card(
         modifier = Modifier
-            //  .padding(5.dp)
+            .padding(5.dp)
             .clickable(onClick = {
                 productSelected = product
                 popUpDetailsOpen.value = true
@@ -37,7 +48,7 @@ fun ImageContent(
                 load = { loadImageBitmap("$URL_HEAD_IMAGES${product.image}") },
                 painterFor = { remember { BitmapPainter(it) } },
                 contentDescription = "Sample",
-                modifier = Modifier.width(200.dp)
+                modifier = Modifier.width(200.dp).height(300.dp)
             )
 
 
